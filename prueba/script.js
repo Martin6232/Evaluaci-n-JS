0d147431ce04475726580d15aa55ad05f1835df5
Martin6232 <martinrodriguez@liceovvh.cl>
2025-04-01 11:11:34 -0300

    ok

--- prueba/script.js
+++ prueba/script.js
@@ -1,7 +1,7 @@
 function impares() {
-    let num = 100;
-    let array = [];
-    for(let n = 1; n <= num; n++){
+    let num = 100;// crea una variable con un valor de 100
+    let array = [];// un array para guardad los numero impares
+    for(let n = 1; n <= num; n++){// crea una var con valor 1 y la condicion es que 
         if(n % 2 == 1){
             array.push(n);
         } else if(n >= num){
