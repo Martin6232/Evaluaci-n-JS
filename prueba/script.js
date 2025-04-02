c03fdfd4aed836098e9066541eb5b5f14653ec80
Martin6232 <martinrodriguez@liceovvh.cl>
2025-04-02 15:32:49 -0300

    ok

--- prueba/script.js
+++ prueba/script.js
@@ -1,10 +1,10 @@
 function impares() {
-    let num = 100;// crea una variable con un valor de 100
-    let array = [];// un array para guardad los numero impares
-    for(let n = 1; n <= num; n++){// crea una var con valor 1, la condicion es que n sea menor o igual a num y cada que se completa una ves el bucle se suma uno mas al num
-        if(n % 2 == 1){// es una condicion para filtrar lus numeros impares
-            array.push(n);// guarda los numeros un el array
-        } else if(n >= num){// una condicion para cuando se complete el bucle imprima los datos del array
+    let num = 100;  // crea una variable con un valor de 100
+    let array = []; // un array para guardad los numero impares
+    for(let n = 1; n <= num; n++){  // crea una var con valor 1, la condicion es que n sea menor o igual a num y cada que se completa una ves el bucle se suma uno mas al num
+        if(n % 2 == 1){ // es una condicion para filtrar lus numeros impares
+            array.push(n);  // guarda los numeros un el array
+        } else if(n >= num){    // una condicion para cuando se complete el bucle imprima los datos del array
             alert(`Los impares del numero 100 son:\n ${array.join(" - ")}`);
         }
     }
