1a893d3e0b8b248a32a0e23cc54bb57c482605b7
Martin6232 <martinrodriguez@liceovvh.cl>
2025-04-03 10:37:34 -0300

    ok

--- prueba/script.js
+++ prueba/script.js
@@ -1,8 +1,8 @@
 function impares() {
-    let array = []; // un array para guardad los numero impares
-    for(let n = 1; n <= 200; n++){ // crea una var con valor 1, la condicion es que n sea menor o igual a 200 proque la mitad de los numeros son par y la otra impar, cada que se completa una ves el bucle se suma uno mas al n
-        if(n % 2 == 1){ // es una condicion para filtrar lus numeros impares
-            array.push(n);  // con .push los numeros filtrados se guardaran en el array 
+    let array = []; 
+    for(let n = 1; n <= 200; n++){ 
+        if(n % 2 == 1){ 
+            array.push(n);  
         } 
     }
     alert(`Los primero 100 numeros impares son:\n ${array.join(" - ")}`);
